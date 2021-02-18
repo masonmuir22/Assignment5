a5ed7a7c5860eebc294b188a51a85af5524b0e4d
masonmuir22 <masonmuir22@gmail.com>
2021-02-18 00:39:33 -0700

    second commit

---
+++ Migrations/20210218072037_PhaseUpdate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Assignment5.Migrations
+{
+    public partial class PhaseUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
